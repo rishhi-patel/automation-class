151326c4210a7d38effa4ec516cae0fa1737135b
Rishi <developer.rishi.99@gmail.com>
2025-01-18 14:53:21 -0500

    -- added base code

---
+++ week2/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+resource "docker_image" "demo_web" {
+  name = "demo-web-app:latest"
+  build {
+    context    = "${path.module}"
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_container" "demo_web_container" {
+  image = docker_image.demo_web.image_id
+  name  = "node-web-app"
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
